20663a1b6676d06df3d17a013a973741991e6d66
Kien Ngo <github@kien.ngo>
2024-10-22 19:17:21 +0700

    Update

--- packages/thirdweb/src/react/web/ui/ConnectWallet/Details.tsx
+++ packages/thirdweb/src/react/web/ui/ConnectWallet/Details.tsx
@@ -170,6 +170,7 @@ export const ConnectedWalletDetails: React.FC<{
         chains={props.chains}
         displayBalanceToken={props.detailsButton?.displayBalanceToken}
         connectOptions={props.connectOptions}
+        swapAssetTabsPositions={props.detailsModal?.swapAssetTabsPositions}
       />,
     );
   }
@@ -286,6 +287,7 @@ function DetailsModal(props: {
   chains: Chain[];
   displayBalanceToken?: Record<number, string>;
   connectOptions: DetailsModalConnectOptions | undefined;
+  swapAssetTabsPositions?: boolean;
 }) {
   const [screen, setScreen] = useState<WalletDetailsModalScreen>("main");
   const { disconnect } = useDisconnect();
@@ -801,6 +803,7 @@ function DetailsModal(props: {
           setScreen={setScreen}
           client={client}
           connectLocale={locale}
+          swapAssetTabsPositions={props.detailsModal?.swapAssetTabsPositions}
         />
       );
     } else {
@@ -1460,6 +1463,12 @@ export type UseWalletDetailsModalOptions = {
    * By default the "Buy Funds" button is shown.
    */
   hideBuyFunds?: boolean;
+
+  /**
+   * When you click on "View Assets", by default the "Tokens" tab is shown first.
+   * If you want to show the "NFTs" tab first, set this prop to `true`
+   */
+  swapAssetTabsPositions?: boolean;
 };
 
 /**
@@ -1517,6 +1526,7 @@ export function useWalletDetailsModal() {
               hideBuyFunds: props.hideBuyFunds,
               hideReceiveFunds: props.hideReceiveFunds,
               hideSendFunds: props.hideSendFunds,
+              swapAssetTabsPositions: props.swapAssetTabsPositions,
             }}
             displayBalanceToken={props.displayBalanceToken}
             theme={props.theme || "dark"}
